cdffd995af75ed46244f297644c280b90345a84e
RimuruTempestCkCK <rimurudotcom@gmail.com>
2026-09-07 15:10:40 +0700

    fix: import Link in BlogDetail to prevent crash

--- src/pages/blog/BlogDetail.jsx
+++ src/pages/blog/BlogDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, Navigate, Link } from 'react-router-dom';
 import useSEO from '../../hooks/useSEO';
 import { blogData } from '../../data/blogData';
 import { kotaData } from '../../data/kotaData';
